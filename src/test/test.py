72e665588f1c3de73c21f8e75391dca4f1644515
Javier Enciso <j4r.e4o@gmail.com>
2018-03-13 09:11:18 -0500

    Add sample test case

--- src/test/test.py
+++ src/test/test.py
@@ -1,1 +1,12 @@
-# TODO: Add you test here
+import unittest
+
+class NGCTestCase(unittest.TestCase):
+    """Tests for `NGC.py`."""
+
+    def test_is_NGC_valid(self):
+        """Is an NGC object valid?"""
+	# TODO
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()

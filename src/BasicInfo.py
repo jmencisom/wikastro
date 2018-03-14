d8c1dd61a5f9abe2d32537b872456682a2ae7da9
fraroca97 <siernal@hotmail.com>
2018-03-13 22:30:44 -0500

    Implementada solución aportada en el Issue #42

---
+++ src/BasicInfo.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+
+class BasicInfo:
+
+	def __init__(self, attributes):
+		"""
+		Receives the attributes in a string and then assigns these values into
+		the class variables.
+
+		Author: Andres Linares
+		Date: 2018-03-03
+		Modified: 2018-03-06 By Francisco Rodríguez.
+		Parameters: String that contains basic info separated by '|'
+		Returns: Nothing.
+		"""
+		self.__attributes = attributes
+		self.__objectType = ""
+		self.__epoch = "J2000"
+		self.__rightAscension = ""
+		self.__declination = ""
+		self.__helioRadialVelocity = ""
+		self.__redShift = ""
+		self.__morphologicalType = ""
+		self.__apparentMagnitude = ""
+		self.__otherNames = ""
+
+		self.__namesReferences = [[" UGC", " [[Uppsala General Catalogue|UGC]]"],
+			[" PGC", " [[Principal Galaxies Catalogue|PGC]]"],
+			[" MCG", " [[Morphological Catalogue of Galaxies|MCG]]"],
+			[" GC", " [[Catalogue of Nebulae and Clusters of Stars|GC]]"],
+			[" CGCG", " [[Catalogue of Galaxies and Clusters of Galaxies|CGCG]]"],
+			[" NGC", " [[New General Catalogue|NGC]]"],
+			[" ESO", " [[ESO]]"],
+			[" Tuc", " [[Bayer designation|&xi; Tuc]]"],
+			[" 1RXS", " [[1RXS]]"],
+			[" 2MASX", " [[2MASX]]"]]
+		self.__obtainValues()
+
+
+
+	def __obtainValues(self):
+		"""
+		Using the variable 'self.__attributes', a string array is obtained
+		and then each individual string is assigned to differents
+		clas variables.
+
+		Author: Andres Linares
+		Date: 2018-03-03
+		Modified: 2018-03-03 by Francisco Rodríguez.
+		Parameters: Nothing.
+		Returns: Nothing.
+		"""
+		separatedText = self.__attributes.split("|")
+		self.__objectType = separatedText[3]
+		self.__rightAscension = separatedText[1]
+		self.__declination = separatedText[2]
+		self.__helioRadialVelocity = separatedText[4]
+		self.__redShift = separatedText[5]
+		self.__morphologicalType = separatedText[7]
+		self.__apparentMagnitude = separatedText[6]
+		self.__otherNames = self.formatOtherNames(separatedText[8])
+
+
+
+	def setAttributes(self, attributes):
+		self.__attributes = attributes
+		self.__obtainValues()
+
+
+
+	def getAttributes(self):
+		return self.__attributes
+
+
+
+	def getObjectType(self):
+		if(self.testEmptiness(self.__objectType, "Object Type")):
+			return ""
+		else:
+			return self.__objectType
+
+
+
+	def getEpoch(self):
+		return self.__epoch
+
+
+
+	def getRightAscension(self):
+		if(self.testEmptiness(self.__rightAscension, "Right Ascension")):
+			return ""
+		else:
+			return self.__rightAscension
+
+
+
+	def getDeclination(self):
+		if(self.testEmptiness(self.__declination, "Declination")):
+			return ""
+		else:
+			return self.__declination
+
+
+
+	def getHelioRadialVelocity(self):
+		if(self.testEmptiness(self.__helioRadialVelocity,
+				"Helio Radial Velocity")):
+			return ""
+		else:
+			return self.__helioRadialVelocity
+
+
+
+	def getRedShift(self):
+		if(self.testEmptiness(self.__redShift, "Red Shift")):
+			return ""
+		else:
+			return self.__redShift
+
+
+
+	def getMorphologicalType(self):
+		if(self.testEmptiness(self.__morphologicalType, "Morphological Type")):
+			return ""
+		else:
+			return self.__morphologicalType
+
+
+
+	def getApparentMagnitude(self):
+		if(self.testEmptiness(self.__apparentMagnitude, "Apparent Magnitude")):
+			return ""
+		else:
+			return self.__apparentMagnitude
+
+
+
+	def getOtherNames(self):
+		if(self.testEmptiness(self.__otherNames, "Other Names")):
+			return ""
+		else:
+			return self.__otherNames
+
+
+
+	def getCoordinates(self):
+		"""
+		This method concatenates the right ascension and the declination
+		into a single string.
+
+		Author: Andres Linares
+		Date: 2018-03-03
+		Modified: Never.
+		Parameters: Nothing.
+		Returns: Coordinates in the form: right_ascension declination.
+		"""
+		return (self.getRightAscension() + " " + self.getDeclination()).split()
+
+
+
+	def formatOtherNames(self, names):
+		"""
+		This method checks that the main name is not included and also 
+		adds the known references.
+
+		Author: Francisco Rodriguez
+		Date: 2018-03-06
+		Modified: Never.
+		Parameters: A string that contains the list of names returned by simbad.
+		Returns: A String that contains the referenced names.
+		"""
+		format_names=names[names.find(",")+1:] #elimina primer nombre (NGC)
+		for ref in self.__namesReferences:
+			format_names=format_names.replace(ref[0], ref[1])
+		return format_names
+
+
+
+	def testEmptiness(self, variable, name):
+		"""
+		This method test the emptiness of a variable. The emptiness is
+		represented by a empty string '' or by this string: '~'. If
+		the variable is empty then True is returned.
+
+		Author: Andres Linares.
+		Date: 2018-03-03
+		Modified: Never.
+		Parameters: Variable to test and name of the variable to print.
+		Returns: True if variable is empty.
+		"""
+		if(variable == "" or variable == "~"):
+			print("There is no info about " + name + ".")
+			return True
+		return False
---
+++ src/WikiGeneration.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+
+from datetime import date
+
+class WikiGeneration:
+
+
+
+	def __init__(self):
+		pass
+
+
+
+	def generateExtraInfo(self, ngc_num, webpage):
+		"""
+		Generates external links section.
+	 
+		Author: Andres Linares
+		Date: 2018-02-14
+		Modified: 2018-03-08 by Andres Linares.
+		Parameters: number of NGC object and url.
+		Returns: String with external links for wikipedia.
+		"""
+		text = ("==External links==\n*[{0} NGC {num} on SIMBAD]\n" +
+			"[[Category:NGC objects|{num}]]").format(webpage, num = ngc_num)
+		return text
+
+
+
+	def __obtainReference(self, ngc_num, webpage):
+		"""
+		Elaborates a reference using the webpage of simbad, the number of the
+		ngc object and today's date.
+
+		Author: Andres Linares.
+		Date: 2018-03-07
+		Modified: 2018-03-08 by Andres Linares.
+		Parameters: Number of ngc object and url of the webpage.
+		Returns: String with the reference.
+		"""
+		reference = ("<ref name=\"simbad\">{{{{cite web\n" +
+		"  |title=SIMBAD Astronomical Database - CDS (Strasbourg)\n" +
+		"  |work=Results for NGC {0}\n" + 
+		"  |url={1}\n" + 
+		"  |accessdate={2}}}}}</ref>").format(ngc_num, webpage,
+				str(self.__obtainActualDate()))
+		return reference
+
+
+
+	def __obtainActualDate(self):
+		"""
+		Obtains actual date by using the datetime python library in the format
+		yyyy-mm-dd
+
+		Author: Andres Linares.
+		Date: 2018-03-07
+		Modified: Never.
+		Parameters: Nothing
+		Returns: Date object with today's date.
+		"""
+		today = date.today()
+		return today
+
+
+
+	def transformToCoordinatesWiki(self, coordinates):
+		"""
+		Using the SKY_TEMPLATE found in wikipedia, the values are placed
+	 	just like the example.  Last value indicates the distance in light
+	 	years (not implemented yet).
+
+		Author: Andres Linares.
+		Date: 2018-02-13
+		Modified: Never.
+		Parameters: String array that contains right ascension in first three positions
+			        and declination in last three.
+		Returns: String to inject into wikipedia.
+		"""
+		text = "{{Sky|"
+		for x in range(0, 6):
+			text = text + coordinates[x]
+			text = text + "|"
+			if x == 2:
+				text = text + "|"
+		text = text + "999}}"
+		return text
+
+
+
+	def transformToWikiBox(self, ngc_num, basicInfo, webpage, imageName,
+		imageCaption):
+		"""
+		This method creates a simple wikibox using the Infobox galaxy template.
+
+		Author: Andres Linares.
+		Date: 2018-02-14
+		Modified: 2018-03-08 by Andres Linares
+		Parameters: number of NGC object, basicInfo object, webpage, image
+			file name and reference of the image file name.
+		Returns: String to paste into wikipedia.
+		"""
+		reference = self.__obtainReference(ngc_num, webpage)
+		ref = "<ref name=\"simbad\" />"
+
+		text = ("{{{{Infobox galaxy\n" +
+			"| name = [[New General Catalogue|NGC]] {num}\n" +
+			"| image = {image}\n" +
+			"| caption = {caption}\n" +
+			"| epoch = [[{epoch}]]{reference}\n" +
+			"| ra = {{{{RA{coorRA}}}}}{ref}\n" +
+			"| dec = {{{{DEC{coorDEC}}}}}{ref}\n" +
+			"| constellation name = \n" +
+			"| z = {z}{ref}\n" +
+			"| h_radial_v = {{{{nowrap|{hrv}[[Metre per second|km/s]]}}}}{ref}\n" +
+			"| type = {type}{ref}\n" +
+			"| appmag_b = {b}{ref}\n" +
+			"| names = {names}{ref}" +
+			"}}}}").format(num = ngc_num, image = imageName,
+				caption = imageCaption, epoch = basicInfo.getEpoch(),
+				reference = reference, ref = ref,
+				coorRA = self.__transformCoordinatesRA(basicInfo.getCoordinates()),
+				coorDEC = self.__transformCoordinatesDEC(basicInfo.getCoordinates()),
+				z = basicInfo.getRedShift(),
+				hrv = basicInfo.getHelioRadialVelocity(),
+				type = basicInfo.getMorphologicalType(),
+				b = basicInfo.getApparentMagnitude(),
+				names = basicInfo.getOtherNames())
+		return text
+
+
+
+	def __transformCoordinatesRA(self, coordinates):
+		text = "|{0}|{1}|{2}".format(coordinates[0], coordinates[1],
+			coordinates[2])
+		return text
+
+
+
+	def __transformCoordinatesDEC(self, coordinates):
+		text = "|{0}|{1}|{2}".format(coordinates[3], coordinates[4],
+			coordinates[5])
+		return text

30a848a8283e3db0c81738080f856296b947abe8
andres-linares <andres.linares.971122@gmail.com>
2018-05-28 20:17:14 -0500

    agregadas dependencias y archivo csv de entrenamiento

---
+++ src/clfs.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
+class classifiers:
+	def __init__(self):
+		pass
+
+	def __train_ngc(self):
+		pass
--- src/wikastro.py
+++ src/wikastro.py
@@ -2,8 +2,19 @@
 
 import sys
 import NGC
+import clfs
 
 def main(argv):
+	"""
+	Main method of the whole program. The objective of the parameters
+	is to run certain script depending of which kind of object is
+	desired to search.
+	There are other options like for example: train the classifiers. In
+	a future could be more.
+
+	:Param: arguments.
+	:Returns: nothing.
+	"""
 	if(len(argv) == 1):
 		if(argv[0].upper() == "--NGC"):
 			NGC.main()
@@ -11,6 +22,10 @@ def main(argv):
 			print("Wikastro v1.0")
 		elif(argv[0].upper() == "--HELP" or argv[0].upper() == "--H"):
 			print_help()
+	elif(len(argv) == 2):
+		if(argv[0].upper() == "--NGC" and argv[1].upper() == "--TRAIN"):
+			clf = clfs()
+			clf.__train_ngc()
 	else:
 		print("Please use an option. For more information type --help")
 

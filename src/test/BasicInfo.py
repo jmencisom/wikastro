b68e76cb226b9303d269f3503610ba5331c55446
Francisco Rodríguez <siernal@hotmail.com>
2018-05-31 16:28:05 -0500

    Hecho por George. Añadidos archivos de pruebas

---
+++ src/test/BasicInfo.py
@@ -0,0 +1,342 @@
+# -*- coding: utf-8 -*-
+
+import os
+import urllib as url_op
+import xlrd
+from astropy.coordinates import SkyCoord, get_constellation
+from astropy import units as u
+from astropy.cosmology import WMAP9 as cosmo
+from astropy.coordinates import Distance
+
+class BasicInfo:
+
+	def __init__(self, attributes):
+		"""
+		Receives the attributes in a string and then assigns these values into
+		the class variables.
+
+		:param: String that contains basic info separated by '|'.
+		:type: String
+		:returns: Nothing.
+		"""
+		self.__attributes = attributes
+		self.__objectType = ""
+		self.__nombre=""
+		self.__epoch = "J2000"
+		self.__rightAscension = ""
+		self.__declination = ""
+		self.__helioRadialVelocity = ""
+		self.__redShift = ""
+		self.__morphologicalType = ""
+		self.__apparentMagnitude = ""
+		self.__otherNames = ""
+
+		self.__namesReferences = [[" UGC", " [[Uppsala General Catalogue|UGC]]"],
+			[" PGC", " [[Principal Galaxies Catalogue|PGC]]"],
+			[" MCG", " [[Morphological Catalogue of Galaxies|MCG]]"],
+			[" GC", " [[Catalogue of Nebulae and Clusters of Stars|GC]]"],
+			[" CGCG", " [[Catalogue of Galaxies and Clusters of Galaxies|CGCG]]"],
+			[" NGC", " [[New General Catalogue|NGC]]"],
+			[" ESO", " [[ESO]]"],
+			[" Tuc", " [[Bayer designation|&xi; Tuc]]"],
+			[" 1RXS", " [[1RXS]]"],
+			[" 2MASX", " [[2MASX]]"]]
+		self.__obtainValues()
+
+
+
+	def __obtainValues(self):
+		"""
+		Using the variable 'self.__attributes', a string array is obtained
+		and then each individual string is assigned to differents
+		clas variables.
+
+		:Param: Nothing.
+		:type: String
+		:returns: Nothing.
+		"""
+		separatedText = self.__attributes.split("|")
+		self.__nombre = separatedText[0]
+		self.__objectType = separatedText[3]
+		self.__rightAscension = separatedText[1]
+		self.__declination = separatedText[2]
+		self.__helioRadialVelocity = separatedText[4]
+		self.__redShift = separatedText[5]
+		self.__morphologicalType = separatedText[7]
+		self.__apparentMagnitude = separatedText[6]
+		self.__otherNames = self.formatOtherNames(separatedText[8])
+
+
+
+	def setAttributes(self, attributes):
+		"""
+		Assign the attributes received in the parameters into the
+		attributes from the class and then calls the method obtainValues()
+		in order to get a better organization.
+
+		parameters: attributes obtained from the simbad database.
+		returns: nothing.
+		"""
+		self.__attributes = attributes
+		self.__obtainValues()
+
+
+
+	def getAttributes(self):
+		"""
+		Obtain the attributes from the class and returns it.
+		The variable attributes is the text recieved from the simbad
+		database.
+
+		parameters: nothing.
+		returns: attributes from the class.
+		"""
+		return self.__attributes
+
+
+
+	def getObjectType(self):
+		"""
+		If the object type is not empty, returns its containt. Else, it returns
+		a string that contains just a emptiness.
+
+		parameters: nothing.
+		returns: object type.
+		"""
+		if(self.testEmptiness(self.__objectType, "Object Type")):
+			return ""
+		else:
+			return self.__objectType
+
+
+
+	def getEpoch(self):
+		"""
+		Returns the epoch of the NGC object, this variable usually contains
+		J2000 epoch.
+
+		parameters: nothing.
+		returns: epoch.
+		"""
+		return self.__epoch
+
+
+
+	def getRightAscension(self):
+		"""
+		Returns the right ascension inside the coordinates of the ngc object.
+		If the variable is empty, then is return an empty string.
+
+		parameters: nothing.
+		returns: right ascension.
+		"""
+		if(self.testEmptiness(self.__rightAscension, "Right Ascension")):
+			return ""
+		else:
+			return self.__rightAscension
+
+
+
+	def getDeclination(self):
+		"""
+		Returns the declination inside the coordinates of the ngc object.
+		If the variable is empty, then is return an empty string.
+
+		parameters: nothing.
+		returns: declination.
+		"""
+		if(self.testEmptiness(self.__declination, "Declination")):
+			return ""
+		else:
+			return self.__declination
+
+
+
+	def getHelioRadialVelocity(self):
+		"""
+		Returns the helio radial velocity also known as hrv of the ngc object.
+		If the variable is empty, then is return an empty string.
+
+		parameters: nothing.
+		returns: helio radial velocity.
+		"""
+		if(self.testEmptiness(self.__helioRadialVelocity,
+				"Helio Radial Velocity")):
+			return ""
+		else:
+			return self.__helioRadialVelocity
+
+
+
+	def getRedShift(self):
+		"""
+		Returns the red shift also known as z of the ngc object.
+		If the variable is empty, then is return an empty string.
+
+		parameters: nothing.
+		returns: red shift.
+		"""
+		if(self.testEmptiness(self.__redShift, "Red Shift")):
+			return ""
+		else:
+			return self.__redShift
+
+
+
+	def getMorphologicalType(self):
+		"""
+		Returns the morphological type of the ngc object that is usually
+		abreviated by the simbad database as some casual objects like
+		galaxies, stars, etc.
+
+		parameters: nothing.
+		returns: morphological type.
+		"""
+		if(self.testEmptiness(self.__morphologicalType, "Morphological Type")):
+			return ""
+		else:
+			return self.__morphologicalType
+
+
+
+	def getApparentMagnitude(self):
+		"""
+		Returns the apparent magnitude also known as B of the ngc object.
+		If the variable is empty, then is return an empty string.
+
+		parameters: nothing.
+		returns: morphological type.
+		"""
+		if(self.testEmptiness(self.__apparentMagnitude, "Apparent Magnitude")):
+			return ""
+		else:
+			return self.__apparentMagnitude
+
+
+
+	def getOtherNames(self):
+		"""
+		Returns the other names of an ngc object. this variable could contain
+		a lot of different not so known names or could be just one single name.
+
+		parameters: nothing.
+		returns: other names of the ngc object as a string.
+		"""
+		if(self.testEmptiness(self.__otherNames, "Other Names")):
+			return ""
+		else:
+			return self.__otherNames
+
+
+
+	def getCoordinates(self):
+		"""
+		This method concatenates the right ascension and the declination
+		into a single string.
+
+		:param: Nothing.
+		:returns: Coordinates in the form: right_ascension declination.
+		:rtype: String
+	
+		"""
+		return (self.getRightAscension() + " " + self.getDeclination()).split()
+
+
+
+	def getConstellation(self):
+		"""
+		Coming soon...
+
+		"""
+		ra=self.getRightAscension()
+		dec=self.getDeclination()
+		ra2=ra.split(" ")
+		dec2=dec.split(" ")
+		ra=ra2[0]+"h"+ra2[1]+"m"+ra2[2]+"s"
+		dec=dec2[0]+"d"+dec2[1]+"m"+dec2[2]+"s"
+		constellation = SkyCoord(ra, dec, frame='icrs')
+		return get_constellation(constellation)
+
+
+
+	def getDistance(self):
+		"""
+		This method returns the distance of the object.
+
+		Author: Lina Leon.
+		Date: 2018-04-08
+		Modified: 2018-05-11 by Lina Mejia
+		Parameters: Nothing.
+		Returns: Float with the distance of the object in lightyears units.
+		"""
+
+		
+		if (self.getRedShift() != ""):
+			r = float(self.getRedShift())
+			d2 = Distance(cosmo.comoving_distance(r), u.lightyear)
+			value = float(str(d2).split()[0])
+			divided = value / 1000000 #1 million
+			if divided > 1:
+				return "{:.2f} M".format(divided)
+			else:
+				return "{:.2f} ".format(value)
+		else:
+			return ""
+
+
+
+	def getDiscovererAndYear(self,ngc_num):
+		"""
+ 		This method returns the discoverer of the object and the
+        year it was discovered.
+
+        Author: Lina Leon.
+        Date: 2018-05-11
+        Modified: Never.
+        Parameters: the number of the object required.
+        Returns: two Strings that contains the discoverer and the year
+        respectively.
+		"""
+		self.ngc_num = int(ngc_num)+8
+		page = 'http://ngcicproject.org/public_HCNGC/Public_HCNGC.xls'
+		url_op.urlretrieve(page, "info.xls")
+		workbook = xlrd.open_workbook("info.xls")
+		sheet = workbook.sheet_by_index(0)
+		discoverer = sheet.cell_value(self.ngc_num,9)
+		year = int(sheet.cell_value(self.ngc_num,10))
+		os.remove("info.xls")
+		return [discoverer, year]
+
+
+	def formatOtherNames(self, names):
+		"""
+		This method checks that the main name is not included and also
+		adds the known references.
+
+		:param: A string that contains the list of names returned by simbad.
+		:type: String.
+		:returns: A String that contains the referenced names.
+		:rtype: String.
+		"""
+		format_names=names[names.find(",")+1:] #elimina primer nombre (NGC)
+		for ref in self.__namesReferences:
+			format_names=format_names.replace(ref[0], ref[1])
+		return format_names
+
+
+
+	def testEmptiness(self, variable, name):
+		"""
+		This method test the emptiness of a variable. The emptiness is
+		represented by a empty string '' or by this string: '~'. If
+		the variable is empty then True is returned.
+
+		:param: Variable to test and name of the variable to print.
+		:type: String
+		:returns: True if variable is empty.
+		:rtype:Boolean.
+		"""
+		if(variable == "" or variable == "~"):
+			print("There is no info about " + name + ".")
+			return True
+		return False
---
+++ src/test/ObtainInfo.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import urllib as url_op
+from bs4 import BeautifulSoup
+
+class ObtainInfo:
+
+	def __init__(self):
+		"""
+		There is no need to initialize the class with any variables.
+		"""
+		pass
+
+
+	def getValues(self, soup):
+		"""
+		Using get_text() method from BeautifulSoup you get a lot of spaces
+		and new line characters, then is required to get rid of them to
+		ease the obtaining labor later.
+
+		Author: Andres Linares.
+		Date: 2018-02-14
+		Modified: 2018-02-27 by Andres Linares.
+		Parameters: BeautifulSoup object
+		Returns: String that contains all the meaning data (basic info in simbad
+		         database).
+		"""
+		childs = soup.get_text()
+		text = ""
+		flag = 0
+		flag2 = 0
+
+		for child in childs:
+			if(child == "\n" and flag == 0):
+				text = text + "\n"
+				flag = 1
+			if(child != " " and child != "\n"):
+				flag = 0
+				flag2 = 0
+				text = text + child
+			if(child == " " and flag == 0):
+				text = text + " "
+				flag2 = 1
+
+		text = text.splitlines()
+		text = text[len(text)-1]
+
+		return text
+
+
+	def obtainPage(self, ngc_num):
+		"""
+		Search a NGC object in simbad database and then returns the query in its
+		full html page.  The search could be empty. This method uses urllib library.
+
+		Author: Andres Linares.
+		Date: 2018-02-13
+		Modified: 2018-02-27 by Andres Linares
+		Parameters: Integer encapsulated as a string.
+		Returns: BeautifulSoup object encoded in 'lxml'
+		"""
+		searchingPt1 = ("http://simbad.u-strasbg.fr/simbad/sim-script?submit=" +
+			"submit+script&script=format+object+form1+%22%25IDLIST%281%29" +
+			"%7C%25COO%28%3BA%3BJ2000%29%7C%25COO%28%3BD%3BJ2000%29%7C+%25OTYPE" +
+			"%28V%29%7C%25RV%28V%29%7C%25RV%28Z%29%7C%25FLUXLIST%28B%3B+F%29%7C" +
+			"%25MT%28M%29%7C%25IDLIST%28SA%28%2c%20%29%3B%29%7C%22%0D%0A" +
+			"query+id+ngc+")
+		searchingPt2 = "%0D%0A"
+		searchingWord = searchingPt1 + ngc_num + searchingPt2
+
+		searchingURL1 = "http://simbad.u-strasbg.fr/simbad/sim-basic?Ident=NGC+"
+		searchingURL2 = "&submit=SIMBAD+search"
+		searchingURLWord = searchingURL1 + ngc_num + searchingURL2
+		webpage = searchingURLWord
+
+		searchingPage = url_op.urlopen(searchingWord)
+		return BeautifulSoup(searchingPage, "lxml"), webpage
